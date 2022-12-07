8bce77cdc2c7a205a09e4803a9c9827cc2960743
saori_tanita <saori_tanita@r.recruit.co.jp>
2022-12-07 18:23:19 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 谷田　沙緒里 on 2022/12/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
